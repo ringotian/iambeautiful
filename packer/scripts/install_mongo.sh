93b9c832a1989195d587d2fad8a136f025d52e23
Anna Yablunovskaya <lad.shada@gmail.com>
2019-12-07 21:28:44 +0300

    create base image for reddit app

--- packer/scripts/install_mongo.sh
+++ packer/scripts/install_mongo.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Install MongoDB
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
 apt-get update
 apt-get install -y mongodb-org
 systemctl enable mongod
